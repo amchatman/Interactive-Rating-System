25418fef540f7af096c4dcbfdbc38fbdd7887cbd
Ashley Chatman <ashchat2008@gmail.com>
2022-07-23 14:26:25 -0400

    Thank page update
    Index.html
    --Delete unused elements
    
    script.js
    --Edit innerHTML
    
    style.css
    --Add style for thank page

--- script.js
+++ script.js
@@ -18,18 +18,19 @@ form.addEventListener("submit", (e) => {
 
 function renderThankyouContent(container, ratingValue) {
   container.innerHTML = `
-    <div class="thank-you">
-        <img
-          src="./assets/illustration-thank-you.svg"
-          alt="thankyou"
-          class="thank-you-img"
-        />
-        <p class="result">You selected ${ratingValue} out of 5</p>
-        <h1 class="thank-you__title">Thank you!</h1>
-        <p class="thank-you__description">
+    <div class="thank-you-container">
+        <img src="./images/undraw_reviews_lp8w.png" alt="thankyou" class="rating-image"/>
+        <div class="rating-result-container">
+          <p class="result">You selected ${ratingValue} out of 5</p>
+        </div>
+        <h1 class="thank-you-title">Thank you!</h1>
+        <p>
           We appreciate you taking the time to give a rating. If you ever need
           more support, don't hesitate to get in touch!
         </p>
+          <div class="back-btn-container">
+            <a href="index.html" class="back-btn">Back</a>
+          </div>
       </div>
     `;
 }
